2451b809a0de4db16a9a438627851d07c6e2388d
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-08-14 23:35:14 -0400

    created very basic controller file functions just to test that everything is set up correctly and that our routes are working fine.

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -1,6 +1,31 @@
-const { workout } = require("./models/workoutModel");
+const workout = require("../models/workoutModel");
 
+// This will be the function to get all workout documents in the database
+const getAllWorkouts = async (req, res) => {
+  res.status(200).json({ message: "in get all workouts" });
+};
 
-const getAllWorkouts = async (req,res)=>{
-    console.log('in here')
-}
+//this will be the function to get a specific workout document
+const getWorkout = async (req, res) => {
+  res.status(200).json({ message: "In getWorkout" });
+};
+
+// this will be to create a workout document
+
+const createWorkout = async (req, res) => {
+  res.status(200).json({ message: "In createWorkout" });
+};
+
+//this will be to update a workout
+const updateWorkout = async (req, res) => {
+  res.status(200).json({ message: "in updateWorkout" });
+};
+
+//this will be to delete a workout document
+
+const deleteWorkout = async (req, res) => {
+  res.status(200).json({ message: "In deleteWorkout" });
+};
+module.exports = {
+  getAllWorkouts,
+};
