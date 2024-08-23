09ac2b7eb0d6ed2d60aed989a5de1caccef3f693
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-08-22 20:58:25 -0400

    added context so when the user deletes a workout it shows the change instantly

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,21 @@
+import { useWorkoutContext } from "./hooks/useWorkoutContext";
 const WorkoutDetails = ({ workout }) => {
-  const handleTitleClick = () => {
-    console.log("hi");
+  const { dispatch } = useWorkoutContext();
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
   };
   return (
     <div>
-      <h3 onClick={handleTitleClick}>{workout.title}</h3>
+      <h3>{workout.title}</h3>
       <p>The amount of reps for this exercise is: {workout.reps}</p>
       <p>The load for this exercise (in pounds) is: {workout.load}</p>
+      <span onClick={handleClick}>Click to delete me</span>
     </div>
   );
 };
