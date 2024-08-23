6dcd8ddebc860b2d49acad83ca6d8235fb828909
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-08-22 20:57:30 -0400

    added link tags so when  a user clicks the logo it takes them to the home page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav>
-      <h1>Workouts4U</h1>
+      <h1>
+        <Link to="/">Workouts4U</Link>
+      </h1>
+
       <Link to="/create">Click me to go somewhere</Link>
     </nav>
   );
