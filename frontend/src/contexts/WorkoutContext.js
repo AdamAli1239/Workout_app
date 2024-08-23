8802f0ae9a15e5bb43b2ed12cdbf9051db89910d
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-08-22 20:56:22 -0400

    created a file for context to make the changes the user makes on the site be seemless

---
+++ frontend/src/contexts/WorkoutContext.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+
+const WorkoutReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUTS":
+      return {
+        workouts: [action.payload, ...state.lmao],
+      };
+
+    case "DELETE_WORKOUT":
+      return {
+        workouts: [
+          ...state.workouts.filter(
+            (workout) => workout._id !== action.payload._id
+          ),
+        ],
+      };
+
+    default:
+      return {
+        workouts: [...state.workouts],
+      };
+  }
+};
+
+export const WorkoutContext = createContext();
+
+export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(WorkoutReducer, {
+    workouts: null,
+  });
+  return (
+    <WorkoutContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutContext.Provider>
+  );
+};
